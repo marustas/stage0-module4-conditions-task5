3c7d7ebb08c03acc43bb29a175841abab3c5cae6
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-04 22:54:00 +0300

    Third try

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,19 +2,16 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-        int remainder = year % 400 == 0 && year % 100 == 0 || year % 4 == 0 ? 1 : 0;
-
-        switch (remainder) {
-            case 1:
-                System.out.println("leap");
-                break;
-            case 0:
-                System.out.println("not leap");
-                break;
-            default:
-                System.out.println("Invalid");
-                break;
-
+        if (year % 400 == 0) {
+            System.out.println("leap");
+        } else if (year % 100 == 0) {
+            System.out.println("not leap");
+        } else if (year % 4 == 0) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
         }
+
     }
 }
+
