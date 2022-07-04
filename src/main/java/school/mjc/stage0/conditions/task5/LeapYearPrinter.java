3550aa2436dbab8faa7c33eef44c992089109640
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-04 22:08:58 +0300

    First try

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-        int remainder = year % 400 == 0 || year % 100 == 0 || year % 4 == 0 ? 1 : 0;
+        int remainder = year % 400 == 0 && year % 100 == 0 || year % 4 == 0 ? 1 : 0;
 
         switch (remainder) {
             case 1:
